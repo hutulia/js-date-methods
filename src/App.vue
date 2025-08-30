b10fd30c3a1a393036faacd32d373a984cf5701f
vitaliy bondarenko <bovile17051992@gmail.com>
2025-08-30 19:51:06 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+  const date = '2025-09-01T00:00:00+00:00';
+  const d = new Date(date);
+  const methods = [
+    'getFullYear',
+    'getMonth',
+    'getDate',
+    'getHours',
+    'getMinutes',
+    'getSeconds',
+    'getMilliseconds',
+    'getDay',
+    'getUTCFullYear',
+    'getUTCMonth',
+    'getUTCDate',
+    'getUTCHours',
+    'getUTCMinutes',
+    'getUTCSeconds',
+    'getUTCMilliseconds',
+    'getUTCDay',
+    'toDateString',
+    'toGMTString',
+    'toISOString',
+    'toJSON',
+    'toLocaleDateString',
+    'toLocaleString',
+    'toLocaleTimeString',
+    'toString',
+    'toTimeString',
+    'toUTCString',
+  ];
+  const data = {};
+  methods.map(m => {
+    data[m] = d[m]();
+  });
+</script>
+
+<template>
+  <input :value="date">
+  <table>
+    <tr v-for="(v,m) in data">
+      <td v-html="m"></td>
+      <td v-html="v"></td>
+    </tr>
+  </table>
+</template>
+
+<style scoped></style>
